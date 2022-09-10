618d0e48900dafc14650b5374d72a1e13800210b
yeison <alexandr.ortega@hotmail.com>
2022-09-10 08:19:33 -0500

    corrigientod

--- MascotaFeliz.App.Consola/Program.cs
+++ MascotaFeliz.App.Consola/Program.cs
@@ -12,7 +12,7 @@ namespace MascotaFeliz.App.Consola
         private static IRepositorioVeterinario _repoVeterinario = new RepositorioVeterinario(new Persistencia.AppContext());
         private static IRepositorioMascota _repoMascota = new RepositorioMascota(new Persistencia.AppContext());
         private static IRepositorioHistoria _repoHistoria = new RepositorioHistoria(new Persistencia.AppContext());
-        private static IRepositorioVisitaPyP _repoVisitaPyP = new RepositorioVisitaPyP(new Persistencia.AppContext());
+        // private static IRepositorioVisitaPyP _repoVisitaPyP = new RepositorioVisitaPyP(new Persistencia.AppContext());
         static void Main(string[] args)
         {
             Console.WriteLine("Hola amigos vamos a empezar a trabajar con las tablas");
