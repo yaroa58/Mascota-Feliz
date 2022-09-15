73231f5cfd850c2c331b0472721a8647ef361a8f
Tigucaro <tigucaro@gmail.com>
2022-09-15 08:43:23 -0500

    Página detalles dueños

--- MascotaFeliz.App.Frontend/Pages/Duenos/DetallesDuenos.cshtml.cs
+++ MascotaFeliz.App.Frontend/Pages/Duenos/DetallesDuenos.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using MascotaFeliz.App.Dominio;
 using MascotaFeliz.App.Persistencia;
 
+
 namespace MascotaFeliz.App.Frontend.Pages
 {
     public class DetallesDuenosModel : PageModel
